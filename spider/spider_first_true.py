8c626637ee4f7277f69ae046b5074a42cef2efd8
可及 <3183346767@qq.com>
2021-10-13 13:11:57 +0800

    进行重试

--- spider/spider_first_true.py
+++ spider/spider_first_true.py
@@ -42,21 +42,36 @@ cursor = database.cursor()
 cursor.execute('select version()')
 data = cursor.fetchone()
 
+index = 20000
+
 # print(data)
-for i in range(1, 10000):
-    print("\n===0.7秒一次哦===============" + str(i) + "====================================\n")
-    time.sleep(0.7)
+while index < 40426:
+    print("\n===0.7秒一次哦===============" + str(index) + "====================================\n")
+    # time.sleep(0.7)
     # 调用api接口
-    url = 'https://api.github.com/repos/' + owner_name + '/' + repo_name + '/pulls/' + i.__str__()
+    # 调用api接口
+    url = 'https://api.github.com/repos/' + owner_name + '/' + repo_name + '/pulls/' + index.__str__()
     r = requests.get(url, headers=headers)
     print("url: " + url + "  Status Code:", r.status_code)
+    try:
+        r = requests.get(url)
+        print("url: " + url + "  Status Code:", r.status_code)
+        index = index + 1
+    except Exception as e:
+        # 如果发生错误则回滚
+        print("网络连接失败: ", url)
+        filename = repo_name+'_PR_network_exception.csv'
+        write_file(index, repo_name, str(e) + url, filename)
+        print(e)
+        continue
+
     # 如果返回的状态码以2开头，则说明正常此时去写入到数据库中即可
     if r.status_code >= 200 and r.status_code < 300:
         json_str = r.json()
         sqlData = (
             json_str['number']
             , json_str['url']
-            ,owner_name+"/"+repo_name
+            , owner_name + "/" + repo_name
             , json_str['user']['id']
             , json_str['user']['login']
             , time_reverse(json_str['created_at'])
@@ -81,13 +96,13 @@ for i in range(1, 10000):
             # 如果发生错误则回滚
             print("插入数据库失败: ", url + "  " + str(json_str['number']))
             filename = repo_name + '_PR_database_operation_exception.csv'
-            write_file(i, repo_name, str(e), filename)
+            write_file(index, repo_name, str(e), filename)
             print(e)
             # traceback.print_exc()
             database.rollback()
     # 如果返回的状态码有问题，则按照问题去处理一下，记录到文件中
     else:
         filename = repo_name + '_PRNumbers_exception.csv'
-        write_file(i, repo_name, r.status_code.__str__() + str(r.json()), filename)
+        write_file(index, repo_name, r.status_code.__str__() + str(r.json()), filename)
 # 关闭数据库连接
 database.close()
