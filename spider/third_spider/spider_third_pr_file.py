819997aa21ec97ab635cc4f2fdc28f8abf009224
可及 <3183346767@qq.com>
2021-11-06 11:33:25 +0800

    获取文件相关信息

--- spider/third_spider/spider_third_pr_file.py
+++ spider/third_spider/spider_third_pr_file.py
@@ -5,154 +5,164 @@ from utils.access_token import get_token
 from utils.exception_handdle import write_file
 import traceback
 import time
+import json
 
-# 获取文件相关内容
-filepath = "E:\\pythonProject\\nju_pr_project\\spider\\second_spider\\user_name_id_except_nodejs.csv"
-user_name_and_id = open(filepath, 'r')
-temp_user_name_id = user_name_and_id.readlines()
-id_name_dict = {}
-user_name = "wozhendeshuai"
-user_id = "node"
-index = 0
 
-# 数据操作部分
-# SQL语句书写
-sql = """INSERT INTO pr_user(
-         user_id,
-         user_name,
-         user_following,
-         user_followers)
-         VALUES(%s,%s,%s,%s) """
+# 统计url中含有的元素数量
+def findUrlJsonCount(url_str):
+    url_str = url_str + "?per_page=100&anon=true&page="
+    print(url_str)
+    page = 1
+    count = 0
+    while 1:
+        temp_url_str = url_str + page.__str__()
+        print(temp_url_str)
+        url_r = requests.get(temp_url_str, headers=headers)
+        url_json = url_r.json()
+        if len(url_json) < 100:
+            count = count + len(url_json)
+            return count
+        else:
+            count = count + 100
+            page = page + 1
+    return count
 
-# 链接数据库
-database = db.connect(host='127.0.0.1', port=3306, user='root', password='root', db='test_pr_first', charset='utf8')
-# 创建游标对象
-cursor = database.cursor()
-# 利用游标对象进行操作
-cursor.execute('select version()')
-data = cursor.fetchone()
+
+# 将list转换为json
+def list_to_json(temp_list):
+    dict = {}
+    for i in range(0, temp_list.__len__()):
+        dict[i] = temp_list[i]
+
+    return json.dumps(dict)
+
+
+# 根据评论条数来取
+def url_to_json(url, number):
+    url_str = url + "?per_page=100&anon=true&page="
+    print(url_str)
+    page = 1
+    maxPage = number // 100 + 1
+    count = 0
+    re_json = json.loads("[]")
+    while page <= maxPage:
+        temp_url_str = url_str + page.__str__()
+        print(temp_url_str)
+        url_r = requests.get(temp_url_str, headers=headers)
+        re_json = re_json + url_r.json()
+        page = page + 1
+    return re_json
+
+
+# https://api.github.com/repos/tensorflow/tensorflow/pulls/872
+# 获取token todo：可以进行选择，不然这样一直是一个会有限制
 access_token = get_token()
 headers = {
     'Authorization': 'token ' + access_token
 }
 
-for i in range(0, len(temp_user_name_id)):
-    temp_val = temp_user_name_id[i]
-    print(temp_val)
-    # 此处是为了去除数据中相关多余的符号
-    val = temp_val.split(",")
-    user_id = val[0].replace("\"", "")
-    user_name = val[1][:len(val[1]) - 1].replace("\"", "")
-    id_name_dict[user_id] = user_name
-    # 用于存储关注和被关注用户信息集合
-    following_id_name_str = ""
-    follower_id_name_str = ""
+# 数据操作部分
+# SQL语句书写
+sql = """
+    insert into pr_file(
+    pr_number, 
+    repo_name, 
+    changed_file_name,
+    sha, 
+    changed_file_status, 
+    lines_added_num, 
+    lines_deleted_num, 
+    lines_changed_num,
+    contain_patch, 
+    patch_content )
+    VALUES( %s,%s,%s, %s,%s, %s,%s, %s,%s, %s )
+   """
+select_sql = """
+    select pr_number,repo_name,changed_file_num,pr_url
+	from pr_self
+	order by pr_number
+	"""
 
-    # 获取该用户关注和被关注的集合url拼接
-    following_url = 'https://api.github.com/users/' + user_name + '/following'
-    # print("following_url", following_url)
-    follower_url = 'https://api.github.com/users/' + user_name + '/followers'
-    # print("follower_url", follower_url)
-    # 调用接口，出错的话进行记录，然后直接跳过该条
-    try:
-        followering_r = requests.get(following_url, headers=headers)
-        print("following_url: " + following_url + "  Status Code:", followering_r.status_code)
-        follower_r = requests.get(follower_url, headers=headers)
-        print("follower_url: " + follower_url + "  Status Code:", follower_r.status_code)
-    except Exception as e:
-        # 如果发生错误则回滚
-        print("网络连接失败: user_name: ", user_name, "user_id: ", user_id)
-        filename = 'user_network_exception.csv'
-        write_file(index, user_id + "-" + user_name, str(e) + ("网络连接失败: user_name: "+ user_name+ "user_id: "+ user_id),
-                   filename)
-        print(e)
-        time.sleep(10)
+# 链接数据库
+database = db.connect(host='127.0.0.1', port=3306, user='root', password='root', db='pr_second', charset='utf8mb4')
+# 创建游标对象
+cursor = database.cursor()
+# 利用游标对象进行操作
+cursor.execute(select_sql)
+data = cursor.fetchall()
+print(data)
+data_len = data.__len__()
+index = 0
+while index < data_len:
+    # 取出查询的数据
+    pr_number = data[index][0]
+    repo_name = data[index][1]
+    file_num = data[index][2]
+    pr_url = data[index][3]
+    print(pr_number)
+    print(repo_name)
+    print(file_num)
+    if file_num == 0:
+        index = index + 1
         continue
-
+    # 拼接url
+    file_url = pr_url + "/files?per_page=100&page="
+    # 调用url中获取
+    maxPage = file_num // 100 + 1
+    page = 1
     try:
-        # 如果返回的状态码以2开头，则说明正常此时去写入到数据库中即可
-        if followering_r.status_code >= 200 and followering_r.status_code < 300:
-            following_json_str = followering_r.json()
-            length_following = len(following_json_str)
-            print("length_following:", length_following)
-            if length_following > 0:
-                for num in range(0, length_following):
-                    following_name = following_json_str[num]['login']
-                    following_id = following_json_str[num]['id']
-                    following_id_name_str = following_id_name_str + (str(following_id) + "-" + following_name + ";")
-            # following无数据，记录到文件中
-            else:
-                filename = 'user_data_exception.csv'
-                write_file(index, user_name,
-                           followering_r.status_code.__str__() + " following none data ",
-                           filename)
-        # 如果返回的状态码有问题，则按照问题去处理一下，记录到文件中
-        else:
-            filename = 'user_data_exception.csv'
-            write_file(index, user_name,
-                       followering_r.status_code.__str__() + " following status ",
-                       filename)
-    except Exception as e:
-        # 如果发生错误则回滚
-        print("第", index, "行 following 数据出现问题", "问题是: ", str(e))
-        filename = 'user_data_exception.csv'
-        write_file(index, user_name, ("第"+ str(index)+ "行数据出现问题"+ "问题是: "+ str(e)), filename)
-        time.sleep(7)
+        while page <= maxPage:
+            print("========================"+"第"+str(index)+"号 pr_number: "+str(pr_number)+" 第"+str(page)+"页"+"==========================")
+            temp_url_str = file_url + page.__str__()
+            print(temp_url_str)
+            url_r = requests.get(temp_url_str, headers=headers)
+            print("url_r: " + temp_url_str + "  Status Code:", url_r.status_code)
+            files_json = url_r.json()
+            #print(files_json)
+            # 遍历整个100行大json，然后逐一放入数据库中即可
+            temp_file_len = files_json.__len__()
+            temp_index = 0
 
-    # 获取该关注该用户的集合
-    try:
-        if follower_r.status_code >= 200 and follower_r.status_code < 300:
-            follower_json_str = follower_r.json()
-            length_follower = len(follower_json_str)
-            print("length_follower:",length_follower)
-            if length_follower > 0:
-                for num in range(0, length_follower):
-                    follower_name = follower_json_str[num]['login']
-                    follower_id = follower_json_str[num]['id']
-                    follower_id_name_str = follower_id_name_str + (str(follower_id) + "-" + follower_name + ";")
-                    # follower无数据，记录到文件中
-                else:
-                    filename = 'user_data_exception.csv'
-                    write_file(index, user_name,
-                               follower_r.status_code.__str__() + " follower none data",
-                               filename)
-            # 如果返回的状态码有问题，则按照问题去处理一下，记录到文件中
-        else:
-            filename = 'user_data_exception.csv'
-            write_file(index, user_name,
-                       follower_r.status_code.__str__() + " following status ",
-                       filename)
-    except Exception as e:
-        # 如果发生错误则回滚
-        print("第", index, "行 follower 数据出现问题", "问题是: ", str(e))
-        filename = 'user_data_exception.csv'
-        write_file(index, user_name, ("第" + str(index) + "行数据出现问题" + "问题是: " + str(e)), filename)
-        time.sleep(7)
+            while temp_index < temp_file_len:
+                try:
+                    sqlData = (
+                        pr_number,
+                        repo_name,
+                        files_json[temp_index]["filename"],
+                        files_json[temp_index]["sha"],
+                        files_json[temp_index]["status"],
+                        files_json[temp_index]["additions"],
+                        files_json[temp_index]["deletions"],
+                        files_json[temp_index]["changes"],
+                        ((files_json[temp_index].__contains__("patch") == True) and 1 or 0),
+                        ((files_json[temp_index].__contains__("patch") == True) and files_json[temp_index].get(
+                            "patch") or None))
+                    # 执行sql语句
+                    cursor.execute(sql, sqlData)
+                    # 提交到数据库执行
+                    database.commit()
+                    print("第"+str(index)+"号 pr_number: "+str(pr_number)+" 第"+str(page)+"页的第" + str(temp_index) + "个数据插入成功")
+                    temp_index = temp_index + 1
+                except Exception as e:
+                    # 如果发生错误则回滚
+                    print("第"+str(index)+"号 pr_number: "+str(pr_number)+" 第"+str(page)+"页的第" + str(temp_index) +"个数据插入数据库失败: " + str(e))
+                    #当出现重复key时应当可以继续往下走，取下一条数据
+                    if e.args[0] == 1062 or e.args[1].__contains__("Duplicate"):
+                        temp_index = temp_index + 1
+                        continue
+                    filename = repo_name + '_file_exception.csv'
+                    write_file(index, "user", ("第" + str(index) + "号 pr_number: " + str(pr_number) + " 第" + str(page) + "页的第" + str(temp_index) + "个数据插入数据库失败: " + str(e)), filename)
+                    print(e)
+                    traceback.print_exc()
+                    database.rollback()
+                    break
+            page = page + 1
 
-    index = index + 1
-
-    try:
-        sqlData = (
-            user_id
-            , user_name
-            , following_id_name_str
-            , follower_id_name_str)
-        # 执行sql语句
-        cursor.execute(sql, sqlData)
-        # 提交到数据库执行
-        database.commit()
-        print("第", index, "行数据插入数据库成功: ", user_name)
     except Exception as e:
-        # 如果发生错误则回滚
-        print("第", index, "行数据插入数据库失败: ", "user_name:", user_name, "user_id:", user_id)
-        filename = 'user_database_operation_exception.csv'
-        write_file(index, "user",
-                   ("第" + str(index) + "行数据插入数据库失败: " + "user_name:" + user_name + "user_id:" + user_id + str(e)),
-                   filename)
+        filename = repo_name + '_file_exception.csv'
+        write_file(index, "user",("第"+str(index)+"号 pr_number: "+str(pr_number)+" 对应的失败: " + str(e)),filename)
         print(e)
-        # traceback.print_exc()
-        database.rollback()
-    print("======================" + str(index) + "=========================")
+        break
+    index = index + 1
 # 关闭数据库连接
 database.close()
-print(len(id_name_dict))
