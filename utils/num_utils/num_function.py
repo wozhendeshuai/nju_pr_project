295a433ac77b8b7a3fc947f339c09ae591cd8da4
可及 <3183346767@qq.com>
2021-11-11 11:31:01 +0800

    num方法增加

--- utils/num_utils/num_function.py
+++ utils/num_utils/num_function.py
@@ -56,3 +56,99 @@ def get_workload(pr_dict):
             re_dict[key] = count
             temp_dict[key] = pr_dict[key]
     return re_dict
+
+
+def get_prev_prs(user_pr_dict):
+    """
+       获取该pr提交之前，提交者提交的pr数量
+       user_pr_dict
+       {
+           52943: {'pr_user_name': 'vict-guo'},
+        }
+    """
+    re_dict = {}
+    temp_dict = {}
+    for key in user_pr_dict.keys():
+        pr_user_name = user_pr_dict[key]['pr_user_name']
+        count = 0
+        if temp_dict.__len__() > 0:
+            for temp_key in temp_dict.keys():
+                temp_user_name = temp_dict[temp_key]['pr_user_name']
+                if temp_user_name == pr_user_name:
+                    count = count + 1
+        temp_dict[key] = user_pr_dict[key]
+        re_dict[key] = count
+    return re_dict
+
+
+def get_change_num(pr_change_dict):
+    """
+       获取该pr提交之前，提交者提交的pr中的代码更改总数
+       user_pr_dict
+       {
+           16380: {'pr_user_name': 'av8ramit', 'changed_line_num': 4227}
+        }
+    """
+    re_dict = {}
+    temp_dict = {}
+    for key in pr_change_dict.keys():
+        pr_user_name = pr_change_dict[key]['pr_user_name']
+        count = 0
+        if temp_dict.__len__() > 0:
+            for temp_key in temp_dict.keys():
+                temp_user_name = temp_dict[temp_key]['pr_user_name']
+                if temp_user_name == pr_user_name:
+                    count = count + temp_dict[temp_key]['changed_line_num']
+        temp_dict[key] = pr_change_dict[key]
+        re_dict[key] = count
+    return re_dict
+
+
+def get_accept_num(pr_dict):
+    """
+       获取该pr提交之前，提交者提交的pr的接受总数
+       user_pr_dict
+       {
+           52946: {'pr_user_name': 'mihaimaruseac', 'merged_at': datetime.datetime(2021, 11, 4, 20, 56, 56)},
+        }
+    """
+    re_dict = {}
+    temp_dict = {}
+    for key in pr_dict.keys():
+        pr_user_name = pr_dict[key]['pr_user_name']
+        count = 0
+        if temp_dict.__len__() > 0:
+            for temp_key in temp_dict.keys():
+                temp_user_name = temp_dict[temp_key]['pr_user_name']
+                temp_merged_at = temp_dict[temp_key]['merged_at']
+                if temp_user_name == pr_user_name and temp_merged_at is not None:
+                    count = count + 1
+        temp_dict[key] = pr_dict[key]
+        re_dict[key] = count
+    return re_dict
+
+def get_close_num(pr_dict):
+    """
+       获取该pr提交之前，提交者提交的pr的关闭总数
+       user_pr_dict
+       {
+           52949: {'pr_user_name': 'tlemo', 'created_time': datetime.datetime(2021, 11, 4, 23, 37, 50), 'closed_time': None, 'merged_at': None},
+        }
+    """
+    re_dict = {}
+    temp_dict = {}
+    for key in pr_dict.keys():
+        pr_user_name = pr_dict[key]['pr_user_name']
+        count = 0
+        if temp_dict.__len__() > 0:
+            created_time = pr_dict[key]['created_time']
+            closed_time = ((pr_dict[key]['closed_time'] is not None) and pr_dict[key]['closed_time'] or datetime.now())
+            for temp_key in temp_dict.keys():
+                temp_user_name = temp_dict[temp_key]['pr_user_name']
+                temp_created_time = temp_dict[temp_key]['created_time']
+                temp_closed_time = ((temp_dict[temp_key]['closed_time'] is not None) and temp_dict[temp_key]['closed_time'] or datetime.now())
+                if temp_user_name == pr_user_name and temp_created_time < created_time < temp_closed_time and temp_closed_time >= closed_time:
+                    count = count + 1
+        temp_dict[key] = pr_dict[key]
+        re_dict[key] = count
+    return re_dict
--- utils/num_utils/test/get_num_for_test.py
+++ utils/num_utils/test/get_num_for_test.py
@@ -1,7 +1,8 @@
 import pymysql as db
 import requests
 
-from utils.num_utils.num_function import get_label_count, get_workload
+from utils.num_utils.num_function import get_label_count, get_workload, get_prev_prs, get_change_num, get_accept_num, \
+    get_close_num
 from utils.time_utils import time_reverse
 from utils.access_token import get_token
 from utils.exception_handdle import write_file
@@ -24,6 +25,7 @@ def test_get_labels():
     print(labels_dict)
     print(get_label_count(labels_dict))
 
+
 def test_get_workload():
     global pushed_time, time_dict, index
     # 利用游标对象进行操作
@@ -40,9 +42,77 @@ def test_get_workload():
     print(pr_dict)
     print(get_workload(pr_dict))
 
+
+def test_get_prev_prs():
+    global pushed_time, time_dict, index
+    # 利用游标对象进行操作
+    cursor.execute(pr_sql)
+    data = cursor.fetchall()
+    print(data.__len__())
+    user_pr_dict = {}
+    index = 0
+    while index < data.__len__():
+        user_pr_dict[data[index][0]] = {}
+        user_pr_dict[data[index][0]]["pr_user_name"] = data[index][5]
+        index = index + 1
+    print(user_pr_dict)
+    print(get_prev_prs(user_pr_dict))
+
+
+def test_get_change_num():
+    global pushed_time, time_dict, index
+    # 利用游标对象进行操作
+    cursor.execute(pr_sql)
+    data = cursor.fetchall()
+    print(data.__len__())
+    user_pr_dict = {}
+    index = 0
+    while index < data.__len__():
+        user_pr_dict[data[index][0]] = {}
+        user_pr_dict[data[index][0]]["pr_user_name"] = data[index][5]
+        user_pr_dict[data[index][0]]["changed_line_num"] = data[index][9] + data[index][10]
+        index = index + 1
+    print(user_pr_dict)
+    print(get_change_num(user_pr_dict))
+
+def test_get_accept_num():
+    global pushed_time, time_dict, index
+    # 利用游标对象进行操作
+    cursor.execute(pr_sql)
+    data = cursor.fetchall()
+    print(data.__len__())
+    user_pr_dict = {}
+    index = 0
+    while index < data.__len__():
+        user_pr_dict[data[index][0]] = {}
+        user_pr_dict[data[index][0]]["pr_user_name"] = data[index][5]
+        user_pr_dict[data[index][0]]["created_time"] = data[index][1]
+        user_pr_dict[data[index][0]]["closed_time"] = data[index][3]
+        user_pr_dict[data[index][0]]["merged_at"] = data[index][4]
+        index = index + 1
+    print(user_pr_dict)
+    print(get_accept_num(user_pr_dict))
+
+def test_get_close_num():
+    global pushed_time, time_dict, index
+    # 利用游标对象进行操作
+    cursor.execute(pr_sql)
+    data = cursor.fetchall()
+    print(data.__len__())
+    user_pr_dict = {}
+    index = 0
+    while index < data.__len__():
+        user_pr_dict[data[index][0]] = {}
+        user_pr_dict[data[index][0]]["pr_user_name"] = data[index][5]
+        user_pr_dict[data[index][0]]["created_time"] = data[index][1]
+        user_pr_dict[data[index][0]]["closed_time"] = data[index][3]
+        user_pr_dict[data[index][0]]["merged_at"] = data[index][4]
+        index = index + 1
+    print(user_pr_dict)
+    print(get_close_num(user_pr_dict))
+
 # 数据操作部分
 # SQL语句书写
-repo_sql = """select * from pr_repo"""
 pr_sql = """
 select 
 pr_number,
@@ -52,12 +122,15 @@ closed_at,
 merged_at,
 pr_user_name,
 labels,
-state
+state,
+changed_file_num,
+total_add_line,
+total_delete_line
  from pr_self"""
 # 链接数据库
 database = db.connect(host='127.0.0.1', port=3306, user='root', password='root', db='pr_second', charset='utf8')
 # 创建游标对象
 cursor = database.cursor()
-test_get_workload()
+test_get_close_num()
 # 关闭数据库连接
 database.close()
