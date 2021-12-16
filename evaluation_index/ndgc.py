c843ab25adfb5a862aebdc1133e892b1e4acc57c
可及 <3183346767@qq.com>
2021-12-16 22:33:39 +0800

    调整仓库结构

---
+++ evaluation_index/ndgc.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+
+def ndcg(rel_true, rel_pred, p=None, form="linear"):
+    """ Returns normalized Discounted Cumulative Gain
+    Args:
+        rel_true (1-D Array): relevance lists for particular user, (n_songs,)
+        rel_pred (1-D Array): predicted relevance lists, (n_pred,)
+        p (int): particular rank position
+        form (string): two types of nDCG formula, 'linear' or 'exponential'
+    Returns:
+        ndcg (float): normalized discounted cumulative gain score [0, 1]
+    """
+    rel_true = np.sort(rel_true)[::-1]
+
+    p = min(len(rel_true), min(len(rel_pred), p))
+
+    # 因为索引是从0开始的，正常应该加1，但是从0开始，log(0+1)则等于无穷大，所以这里面加的是2，如果索引是从1开始，则加的是1，所以感觉跟上面的公式不一致，其实是一样的。
+    discount = 1 / (np.log2(np.arange(p) + 2))
+
+    if form == "linear":
+        idcg = np.sum(rel_true[:p] * discount)
+        dcg = np.sum(rel_pred[:p] * discount)
+        print(rel_true)
+    elif form == "exponential" or form == "exp":
+        idcg = np.sum([2 ** x - 1 for x in rel_true[:p]] * discount)
+        dcg = np.sum([2 ** x - 1 for x in rel_pred[:p]] * discount)
+    else:
+        raise ValueError("Only supported for two formula, 'linear' or 'exp'")
+
+    return dcg / idcg
