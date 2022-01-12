af5191bf251ab03243d41cf7928679ac937ca215
可及 <3183346767@qq.com>
2022-01-13 00:12:54 +0800

    结果上传

---
+++ utils/print_photo.py
@@ -0,0 +1,21 @@
+
+def showBN(model, save=False, filename=None,directory=None):
+    '''传入BayesianModel对象，调用graphviz绘制结构图，jupyter中可直接显示'''
+    from graphviz import Digraph
+    node_attr = dict(
+        style='filled',
+        shape='box',
+        align='left',
+        fontsize='12',
+        ranksep='0.1',
+        height='0.2'
+    )
+    dot = Digraph(node_attr=node_attr, graph_attr=dict(size="12,12"))
+    seen = set()
+    edges = model.edges()
+    for a, b in edges:
+        dot.edge(a, b)
+    if save:
+        dot.view(cleanup=True, filename=filename)
+
+    return dot
