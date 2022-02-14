b726798fe0c8b2b7d0b61b42508cdc6bae667e01
wozhendeshuai <jiafeng1997@gmail.com>
2022-02-14 19:34:00 +0800

    移动一下

--- utils/access_token.py
+++ utils/access_token.py
@@ -1,6 +1,6 @@
 # 获取本地文件中的access_tocken相关值
 def get_token(num=0):
-    file = open('F:\\access_token.txt', 'r')
+    file = open('./token.txt', 'r')
     list = file.readlines()
     lines = len(list)
     #str = file.readline()
